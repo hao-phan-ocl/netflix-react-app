781d3c418fad3f3f2926929bcab6c12f2013d128
Hao Phan <nguyenhao.phan@gmail.com>
2021-10-29 21:19:57 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.scss';
-import Billboard from './components/Billboard/Billboard';
 import Home from './pages/Home/Home';
 
 export default function App() {
