8d2393143c015c37c8a8f1de1505ccc95527c717
Hao Phan <nguyenhao.phan@gmail.com>
2021-11-04 20:04:12 +0200

    updated

--- src/customHooks/useModal.js
+++ src/customHooks/useModal.js
@@ -6,7 +6,6 @@ export default function useModal() {
     useEffect(() => {
         openModal && (document.body.style.overflow = 'hidden');
         !openModal && (document.body.style.overflow = 'unset');
-        // document.getElementById('modal-container').onclick = setOpenModal(false);
     }, [openModal]);
 
     return [openModal, setOpenModal]
