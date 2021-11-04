bff53d56be6cef32396e59c4f751a1e792e8b8ac
Hao Phan <nguyenhao.phan@gmail.com>
2021-11-04 02:40:34 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.scss';
-import { ContextProvider } from "./context/UserContext";
 import Home from './pages/Home/Home';
 
 export default function App() {
   return (
-    <ContextProvider>
-      <Home />
-    </ContextProvider>
+    <Home />
   )
 }
