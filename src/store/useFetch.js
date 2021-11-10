f9aec222b1f14331ce8ffe3d4faa98e6f17c4daf
Hao Phan <nguyenhao.phan@gmail.com>
2021-11-11 01:00:40 +0200

    updated

---
+++ src/store/useFetch.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import instance from "../api/axiosInstance";
+
+export default function useFetch(url, isBillboard) {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {       
+        async function fetchData() {
+            const request = await instance.get(url);
+
+            isBillboard ? 
+            setData(request.data.results[Math.floor(Math.random() * request.data.results.length)]) :
+            setData(request.data.results)           
+        }
+        fetchData();
+    }, [url, isBillboard]);
+
+    return [data, setData]
+}
