5a1b296e75b3b785f5c5a905f5b5f292c5db197c
Hao Phan <nguyenhao.phan@gmail.com>
2021-11-14 01:23:35 +0200

    updated

--- src/api/requests.js
+++ src/api/requests.js
@@ -4,13 +4,13 @@ const requests = {
     billboardImage: `https://image.tmdb.org/t/p/original/`,
     slideImage: `https://image.tmdb.org/t/p/w500/`,
 
-    trending: `/trending/all/day?api_key=${apiKey}`,
-    movieTrending: `/trending/movie/day?api_key=${apiKey}`,
-    tvTrending: `/trending/tv/day?api_key=${apiKey}`,
+    trending: `/trending/all/week?api_key=${apiKey}&include_adult=false`,
+    movieTrending: `/trending/movie/week?api_key=${apiKey}&include_adult=false`,
+    tvTrending: `/trending/tv/week?api_key=${apiKey}&include_adult=false`,
 
     movie_genres: `/genre/movie/list?api_key=${apiKey}&language=en-US`,
     tv_genres: `/genre/tv/list?api_key=${apiKey}&language=en-US`,
-    search: `/search/multi?api_key=${apiKey}&language=en-US&query=`
+    search: `/search/multi?api_key=${apiKey}&include_adult=false&language=en-US&query=`
 }
 
 export default requests;
