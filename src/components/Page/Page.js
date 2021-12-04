168e0c507d82217d35373b508153b403f7e3250d
Hao Phan <nguyenhao.phan@gmail.com>
2021-12-04 16:37:48 +0200

    Update Page.js

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,15 +1,13 @@
-import Billboard from '../Billboard/Billboard';
-import Carousel from '../Carousel/Carousel';
-import useFetch from '../../store/useFetch';
-import Nav from '../Nav/Nav';
+import Billboard from '../Billboard/Billboard'
+import Carousel from '../Carousel/Carousel'
+import useFetch from '../../store/useFetch'
 
 export default function Page({fetchData}) {
-    const [billboardData] = useFetch(`${fetchData}`, true);
-    const [carouselData] = useFetch(`${fetchData}`, false);
+    const [billboardData] = useFetch(`${fetchData}`, true)
+    const [carouselData] = useFetch(`${fetchData}`, false)
 
     return (
         <div className="page"> 
-            <Nav />
             <Billboard data={billboardData} />
             <Carousel data={carouselData} />
         </div>
