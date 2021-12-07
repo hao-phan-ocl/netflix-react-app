d617c1cb4fdef7f672861bc97023aa3546ac6704
Hao Phan <nguyenhao.phan@gmail.com>
2021-12-07 02:21:10 +0200

    added loading page

---
+++ src/components/LoadingPage/LoadingPage.js
@@ -0,0 +1,16 @@
+
+export default function LoadingPage() {
+    return (
+        <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'white',
+            height: '80vh',
+            fontSize: '4rem',
+            textShadow: '#000 0 0 10px'
+        }}>
+            Loading...    
+        </div>
+    )
+}
