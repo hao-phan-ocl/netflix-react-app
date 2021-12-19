8517beca4cd27d6cdf0edeb47f9093f3e3c734a2
Hao Phan <nguyenhao.phan@gmail.com>
2021-12-19 04:02:34 +0200

    fixed fetch api for Video component

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,17 +1,39 @@
+import { useEffect, useState } from 'react'
 import reactDom from 'react-dom'
 import ReactPlayer from 'react-player'
+import instance from '../../api/axiosInstance'
 import { requests } from '../../api/requests'
-import useFetch from '../../store/useFetch'
 import './Video.scss'
 
 export default function Video({data, setOpenVideo}) {
-    const [videos] = useFetch(`${'movie' || 'tv'}/${data.id}` + requests.video)
-    const youtubeKey = videos[Math.floor(Math.random() * videos.length)]?.key
+    const [video, setVideo] = useState([])
+    
+    useEffect(() => {
+        async function fetchVideo () {
+            const res = await instance.get(
+                `movie/${data.id}` + requests.video
+            )
+            if (res)
+            setVideo(res?.data.results[Math.floor(Math.random() * res.data.results.length)]?.key)
+        }
+        fetchVideo()
+    }, [data])
+
+    useEffect(() => {
+        async function fetchVideo () {
+            const res = await instance.get(
+                `tv/${data.id}` + requests.video
+            )
+            if (res) 
+            setVideo(res?.data.results[Math.floor(Math.random() * res.data.results.length)]?.key)
+        }
+        fetchVideo()
+    }, [data])
 
     return reactDom.createPortal(
         <div className='player' onClick={() => setOpenVideo(false)}>
             <div className="player-content">
-                {youtubeKey && <ReactPlayer width='100%' height='100%' controls url={`https://www.youtube.com/watch?v=${youtubeKey}`} />}
+                {video && <ReactPlayer width='100%' height='100%' controls url={`https://www.youtube.com/watch?v=${video}`} />}
             </div>
         </div>,
         document.getElementById('modal')
