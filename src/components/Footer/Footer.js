5716fb5e26a991c366e0cb9078066361b8418899
Hao Phan <nguyenhao.phan@gmail.com>
2021-12-28 03:04:58 +0200

    updated gap for flexbox

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,26 +5,28 @@ export default function Footer() {
     return (
         <div className="footer">
             <div className="footer-wrapper">
-                <span className='span-1'>Developed by</span>
-                <span className='span-2'>Hao Phan</span>
-            </div>
-            <div className="icon-box">
-                <a
-                    className='icon' 
-                    href='https://www.linkedin.com/in/hao-phan-06b628110/'
-                    target='_blank'
-                    rel="noreferrer"
-                >
-                    <LinkedIn />
-                </a>
-                <a  
-                    className='icon'
-                    href='https://github.com/nguyenhaophan'
-                    target='_blank'
-                    rel="noreferrer"
-                >
-                    <GitHub />
-                </a>
+                <div className="text-box">
+                    <span className='span-1'>Developed by</span>
+                    <span className='span-2'>Hao Phan</span>
+                </div>
+                <div className="icon-box">
+                    <a
+                        className='icon' 
+                        href='https://www.linkedin.com/in/hao-phan-06b628110/'
+                        target='_blank'
+                        rel="noreferrer"
+                    >
+                        <LinkedIn />
+                    </a>
+                    <a  
+                        className='icon'
+                        href='https://github.com/nguyenhaophan'
+                        target='_blank'
+                        rel="noreferrer"
+                    >
+                        <GitHub />
+                    </a>
+                </div>
             </div>
         </div>
     )
