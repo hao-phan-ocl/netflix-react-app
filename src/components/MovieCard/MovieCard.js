0f70b4aeb3cc4d64f49c1917ac29157f96e1b2c7
Hao Phan <nguyenhao.phan@gmail.com>
2022-03-30 01:30:25 +0300

    updated movie card

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -3,18 +3,18 @@ import InfoButton from '../InfoButton/InfoButton'
 import PlayButton from '../PlayButton/PlayButton'
 import './MovieCard.scss'
 
-export default function MovieCard({data}) {
-    return (
-        <div className='movie-card'>
-            <img 
-                className="image"
-                src={requests.billboardImage + data?.backdrop_path} 
-                alt={data?.name || data?.title}
-            />
-            <div className="button-box">
-                <PlayButton buttonSize={'small'} data={data} />
-                <InfoButton buttonSize={'small'} data={data} />
-            </div>
-        </div>
-    )
+export default function MovieCard({ data }) {
+  return (
+    <div className="movie-card">
+      <img
+        className="image"
+        src={requests.billboardImage + data?.backdrop_path}
+        alt={data?.name || data?.title}
+      />
+      <div className="button-box">
+        <PlayButton buttonSize={'small'} data={data} />
+        <InfoButton buttonSize={'small'} data={data} />
+      </div>
+    </div>
+  )
 }
