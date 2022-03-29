0be02b66f637ce79d42fc31806f943ea5ad40e94
Hao Phan <nguyenhao.phan@gmail.com>
2022-03-30 01:29:34 +0300

    updated modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -9,89 +9,80 @@ import { UserContext } from '../../store/UserContext'
 import './Modal.scss'
 
 export default function Modal({ setOpenModal, data }) {
-    const [genres, setGenres] = useState([])
-    const {addMovie, removeMovie, watchlist} = useContext(UserContext)
+  const [genres, setGenres] = useState([])
+  const { addMovie, removeMovie, watchlist } = useContext(UserContext)
 
-    const inWatchlist = watchlist?.map(elem => elem.id).includes(data.id)
-    
-    useEffect(() => {
-        async function fetchGenres () {
-            const res = await instance.get(
-                requests.movie_genres || 
-                requests.tv_genres
-            )
-            setGenres(
-                res.data.genres.filter(elem => data?.genre_ids.includes(elem.id))
-            )
-        }
-        fetchGenres()
-    }, [data])
-    
-    async function handleAdd() {
-        try {
-            await addMovie(data)
-        } catch (err) {
-            console.log(err)
-        }
+  const inWatchlist = watchlist?.map((elem) => elem.id).includes(data.id)
+
+  useEffect(() => {
+    async function fetchGenres() {
+      const res = await instance.get(requests.movie_genres || requests.tv_genres)
+      setGenres(res.data.genres.filter((elem) => data?.genre_ids.includes(elem.id)))
     }
+    fetchGenres()
+  }, [data])
 
-    async function handleRemove() {
-        try {
-            await removeMovie(data)
-        } catch (err) {
-            console.log(err)
-        }
+  async function handleAdd() {
+    try {
+      await addMovie(data)
+    } catch (err) {
+      console.log(err)
     }
+  }
 
-    return reactDom.createPortal(
-        <div className="modal">
-            <div className="modal-content">
-                <Cancel className="close" onClick={() => setOpenModal(false)}/>
-                <div className="banner-and-info">
-                    <img 
-                        className="banner"
-                        src={`https://image.tmdb.org/t/p/original/${data.backdrop_path}`} 
-                        alt={data?.name || data?.title}
-                    />
-                    <div className="info-container">
-                        <h1 className="title">
-                            {data?.name || data?.title}
-                        </h1>
-                        <div className="button-box">
-                            <PlayButton buttonSize={'large'} data={data} />
-                            {inWatchlist ? (
-                                <div className='add'>
-                                    <CheckCircleOutline onClick={handleRemove} />
-                                    <div className="add-hover" style={{width: '7rem', left: '-2.5rem'}}>
-                                        Remove from My List
-                                    </div>
-                                </div>
-                            ) : (
-                                <div className='add'>
-                                    <AddCircleOutline onClick={handleAdd} />
-                                    <div className="add-hover" style={{width: '5.2rem', left: '-1.7rem'}}>
-                                        Add to My List
-                                    </div>
-                                </div>
-                            )}
-                        </div>
-                    </div>
+  async function handleRemove() {
+    try {
+      await removeMovie(data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  return reactDom.createPortal(
+    <div className="modal">
+      <div className="modal-content">
+        <Cancel className="close" onClick={() => setOpenModal(false)} />
+        <div className="banner-and-info">
+          <img
+            className="banner"
+            src={`https://image.tmdb.org/t/p/original/${data.backdrop_path}`}
+            alt={data?.name || data?.title}
+          />
+          <div className="info-container">
+            <h1 className="title">{data?.name || data?.title}</h1>
+            <div className="button-box">
+              <PlayButton buttonSize={'large'} data={data} />
+              {inWatchlist ? (
+                <div className="add">
+                  <CheckCircleOutline onClick={handleRemove} />
+                  <div className="add-hover" style={{ width: '7rem', left: '-2.5rem' }}>
+                    Remove from My List
+                  </div>
                 </div>
-                <div className="movie-info">
-                    <div className="description">
-                        {data?.overview}
-                    </div>
-                    <div className="genres">
-                        <span style={{color: '#777'}}>Genres: </span>
-                        <span>
-                            {genres.map((genre, i, arr) => {
-                                return i !== (arr.length-1) ? genre.name + ', ' : genre.name
-                            })}
-                        </span>
-                    </div>
+              ) : (
+                <div className="add">
+                  <AddCircleOutline onClick={handleAdd} />
+                  <div className="add-hover" style={{ width: '5.2rem', left: '-1.7rem' }}>
+                    Add to My List
+                  </div>
                 </div>
+              )}
             </div>
-        </div>,
-        document.getElementById('modal')
-    )
+          </div>
+        </div>
+        <div className="movie-info">
+          <div className="description">{data?.overview}</div>
+          <div className="genres">
+            <span style={{ color: '#777' }}>Genres: </span>
+            <span>
+              {genres.map((genre, i, arr) => {
+                return i !== arr.length - 1 ? genre.name + ', ' : genre.name
+              })}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>,
+    document.getElementById('modal')
+  )
 }
