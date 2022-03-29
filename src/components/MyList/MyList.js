511bdf091855318440d5bfd7e8f829e4292b4b35
Hao Phan <nguyenhao.phan@gmail.com>
2022-03-30 01:26:05 +0300

    added prettier

--- src/components/MyList/MyList.js
+++ src/components/MyList/MyList.js
@@ -1,41 +1,41 @@
-import { Container } from "@material-ui/core"
-import { useContext } from "react"
+import { Container } from '@material-ui/core'
+import { useContext } from 'react'
 
-import MovieCard from "../MovieCard/MovieCard"
-import { UserContext } from "../../store/UserContext"
+import MovieCard from '../MovieCard/MovieCard'
+import { UserContext } from '../../store/UserContext'
 
 export default function MyList() {
-    const {watchlist} = useContext(UserContext)
-    
-    return (
-        !watchlist.length ? (
-            <div style={{
-                color: 'white',
-                marginTop: '7rem',
-                marginLeft: '3rem',
-                fontSize: '3rem',
-                textShadow: '#000 0 0 10px'
-            }}>
-                Your watchlist is empty...
-            </div>
-        ) : (
-            <Container maxWidth={'xl'} style={{padding: '8rem 1.5rem'}}>
-                <div 
-                    className="my-grid"
-                    style={{
-                        display: 'grid',
-                        gridTemplateColumns: 'repeat(auto-fill, 300px)',
-                        gridAutoRow: 'auto',
-                        rowGap: '70px',
-                        columnGap: '5px',
-                        justifyContent: 'start'
-                    }}
-                >
-                    {watchlist?.map(elem => (
-                        <MovieCard data={elem} key={elem.id} />
-                    ))}
-                </div>
-            </Container>
-        )
-    )
+  const { watchlist } = useContext(UserContext)
+
+  return !watchlist.length ? (
+    <div
+      style={{
+        color: 'white',
+        marginTop: '7rem',
+        marginLeft: '3rem',
+        fontSize: '3rem',
+        textShadow: '#000 0 0 10px',
+      }}
+    >
+      Your watchlist is empty...
+    </div>
+  ) : (
+    <Container maxWidth={'xl'} style={{ padding: '8rem 1.5rem' }}>
+      <div
+        className="my-grid"
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(auto-fill, 300px)',
+          gridAutoRow: 'auto',
+          rowGap: '70px',
+          columnGap: '5px',
+          justifyContent: 'start',
+        }}
+      >
+        {watchlist?.map((elem) => (
+          <MovieCard data={elem} key={elem.id} />
+        ))}
+      </div>
+    </Container>
+  )
 }
