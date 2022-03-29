54b732ff60385b308021cb5469c7475f9bb6a305
Hao Phan <nguyenhao.phan@gmail.com>
2022-03-30 01:28:19 +0300

    updated loading page

--- src/components/LoadingPage/LoadingPage.js
+++ src/components/LoadingPage/LoadingPage.js
@@ -1,15 +1,17 @@
 export default function LoadingPage() {
-    return (
-        <div style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            color: 'white',
-            height: '80vh',
-            fontSize: '4rem',
-            textShadow: '#000 0 0 10px'
-        }}>
-            Loading...    
-        </div>
-    )
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: 'white',
+        height: '80vh',
+        fontSize: '4rem',
+        textShadow: '#000 0 0 10px',
+      }}
+    >
+      Loading...
+    </div>
+  )
 }
